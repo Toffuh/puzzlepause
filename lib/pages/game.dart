43a0b2bdc663ad5eaa00ef5397cb15d74c44a7ad
to_binio <tobias.frischmann1@gmail.com>
2023-12-09 12:14:31 +0100

    drag & drop test

--- lib/pages/game.dart
+++ lib/pages/game.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class Game extends StatelessWidget {
+class Game extends StatefulWidget {
   const Game({super.key});
 
   @override
+  State<Game> createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  List<String> items = ["a", "c", "b"];
+  List<String?> setItems = [null, null, null];
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          Column(
+            children: [
+              for (var (index, item) in setItems.indexed)
+                DragTarget<String>(
+                  builder: (BuildContext context, List<String?> candidateData,
+                      List<dynamic> rejectedData) {
+                    return SizedBox(
+                        height: 20, width: 200, child: Text(item ?? "nothing"));
+                  },
+                  onAccept: (item) {
+                    print(item);
+                    print(index);
+
+                    setState(() {
+                      setItems[index] = item;
+                    });
+                  },
+                )
+            ],
+          ),
+          Column(
+            children: [
+              for (var value in items)
+                Draggable<String>(
+                  data: value,
+                  feedback: Text(value),
+                  child: Text(value),
+                )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
