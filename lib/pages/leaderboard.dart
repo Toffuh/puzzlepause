00a2ee8b9a7cb2715672e9ded26b1fb2b7c63ea3
benni043 <benni.huff@gmail.com>
2023-12-27 18:28:30 +0100

    added basic leaderboard layout

--- lib/pages/leaderboard.dart
+++ lib/pages/leaderboard.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Leaderboard extends StatelessWidget {
   const Leaderboard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 31, 16, 42),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+                padding: const EdgeInsets.all(30),
+                child: Stack(
+                  children: [
+                    TextButton(
+                        onPressed: () => {Navigator.pop(context)},
+                        child: const Icon(Icons.arrow_back)),
+                    const Center(
+                      child: Text("PuzzlePause - Bestenliste",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30)),
+                    )
+                  ],
+                )),
+          ],
+        ),
+      ),
+    );
   }
 }
