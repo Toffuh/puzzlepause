2b56661d7ca1722ebc6c02a6ca46e22791c69703
benni043 <benni.huff@gmail.com>
2023-12-30 12:32:30 +0100

    implemented basic google login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:puzzelpause/auth/googleAuth.dart';
+import 'package:puzzelpause/auth/googleAuthClass.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
